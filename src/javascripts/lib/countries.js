39f76ce7df511116eb637564fbc167ee9b8f8a3b
David McClure <davidwilliammcclure@gmail.com>
2015-08-14 08:53:00 -0700

    First draft of the rbush indexing logic in Countries.

--- src/javascripts/lib/countries.js
+++ src/javascripts/lib/countries.js
@@ -2,6 +2,8 @@
 
 import turf from 'turf';
 import * as utils from '../utils';
+import rbush from 'rbush';
+import knn from 'rbush-knn';
 
 
 export default class Countries {
@@ -14,6 +16,48 @@ export default class Countries {
    */
   constructor(json) {
     this.json = json;
+    this._indexPoints();
+  }
+
+
+  /**
+   * Index border points into an rtree.
+   */
+  _indexPoints() {
+
+    let points = [];
+
+    // Gather points into a single array.
+    for (let {
+      geometry: {
+        coordinates: coords,
+        type: type
+      }
+    } of this.json.features) {
+      switch (type) {
+
+        case 'Polygon':
+          points.push.apply(points, coords[0]);
+        break;
+
+        case 'MultiPolygon':
+          for (let [polygon] of coords) {
+            points.push.apply(points, polygon);
+          }
+        break;
+
+      }
+    }
+
+    // XY -> XYXY.
+    for (let p of points) {
+      p.push.apply(p, p);
+    }
+
+    // Index the R tree.
+    this.tree = rbush();
+    this.tree.load(points);
+
   }
 
 

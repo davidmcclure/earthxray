bf2427fef3e845a8e1804a305aabee29ca67b353
David McClure <davidwilliammcclure@gmail.com>
2015-08-14 09:15:41 -0700

    Blocking in logic for the rtree-backed xyzToCountry() method.

--- src/javascripts/lib/countries.js
+++ src/javascripts/lib/countries.js
@@ -27,35 +27,46 @@ export default class Countries {
 
     let points = [];
 
-    // Gather points into a single array.
-    for (let {
-      geometry: {
-        coordinates: coords,
-        type: type
-      }
-    } of this.json.features) {
-      switch (type) {
+    // Gather points into an array.
+    for (let f of this.json.features) {
+
+      let coords = [];
+
+      switch (f.geometry.type) {
 
         case 'Polygon':
-          points.push.apply(points, coords[0]);
+          coords.push.apply(coords, f.geometry.coordinates[0]);
         break;
 
         case 'MultiPolygon':
-          for (let [polygon] of coords) {
-            points.push.apply(points, polygon);
+          for (let [polygon] of f.geometry.coordinates) {
+            coords.push.apply(coords, polygon);
           }
         break;
 
       }
+
+      // Link the feature with the point.
+      for (let c of coords) {
+        points.push({
+          minX: c[0],
+          minY: c[1],
+          maxX: c[0],
+          maxY: c[1],
+          feature: f,
+        });
+      }
+
     }
 
-    // XY -> XYXY.
-    for (let p of points) {
-      p.push.apply(p, p);
-    }
+    this.tree = rbush(10, [
+      '.minX',
+      '.minY',
+      '.maxX',
+      '.maxY'
+    ]);
 
     // Index the R tree.
-    this.tree = rbush();
     this.tree.load(points);
 
   }
@@ -69,7 +80,7 @@ export default class Countries {
    * @param {Number} z
    * @return {String}
    */
-  xyzToCountry(x, y, z) {
+  xyzToCountry1(x, y, z) {
 
     // XYZ -> lon/lat.
     let [lon, lat] = utils.xyzToLonLat(x, y, z);
@@ -93,6 +104,31 @@ export default class Countries {
     }
 
     return country;
+
+  }
+
+
+  /**
+   * Given a XYZ coordinate, try to find a country that contains it.
+   *
+   * @param {Number} x
+   * @param {Number} y
+   * @param {Number} z
+   * @return {String}
+   */
+  xyzToCountry2(x, y, z) {
+
+    // XYZ -> lon/lat.
+    let [lon, lat] = utils.xyzToLonLat(x, y, z);
+
+    let point = {
+      geometry: {
+        type: 'Point',
+        coordinates: [lat, lon]
+      }
+    };
+
+    let nn = knn(this.tree, [lon, lat], 5);
 
   }
 

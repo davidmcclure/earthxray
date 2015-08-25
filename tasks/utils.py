0a077198b551d16ecea50e8036cb0d67e48ff84b
David McClure <davidwilliammcclure@gmail.com>
2015-08-25 09:02:24 -0700

    Commenting.

--- tasks/utils.py
+++ tasks/utils.py
@@ -7,6 +7,14 @@ def lon_lat_to_xyz(lon, lat, r=6371):
 
     """
     Convert lon/lat -> XYZ.
+
+    Args:
+        lon (float)
+        lat (float)
+        r (float)
+
+    Returns:
+        list: [X, Y, Z]
     """
 
     # Degrees -> radians.

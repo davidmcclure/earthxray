0c8f54370bebfc637f9a651ef8a0c4f8440a2e15
David McClure <davidwilliammcclure@gmail.com>
2015-09-05 16:03:26 -0700

    Adds a cca3_to_population() helper.

--- tasks/countries.py
+++ tasks/countries.py
@@ -7,18 +7,30 @@ from invoke import task
 
 @task
 def test():
-    print(cca3_to_anchor())
+    print(cca3_to_area())
 
 
 def open_mledoze():
 
     """
-    Read the country metadata.
+    Read the meldoze metadata.
 
     Returns: dict
     """
 
     with open('data/countries/mledoze.json', 'r') as fh:
+        return json.loads(fh.read())
+
+
+def open_population():
+
+    """
+    Read the OKFN population data.
+
+    Returns: dict
+    """
+
+    with open('data/countries/population.json', 'r') as fh:
         return json.loads(fh.read())
 
 
@@ -31,7 +43,6 @@ def cca3_to_area():
     """
 
     areas = {}
-
     for c in open_mledoze():
         areas[c['cca3']] = c['area']
 
@@ -47,8 +58,26 @@ def cca3_to_anchor():
     """
 
     anchors = {}
-
     for c in open_mledoze():
         anchors[c['cca3']] = c['latlng']
 
     return anchors
+
+
+def cca3_to_population():
+
+    """
+    Map country code -> population.
+
+    Returns: dict
+    """
+
+    # Filter out pre-2014 values.
+    data = open_population()
+    current = [d for d in data if d['Year'] == '2014']
+
+    pops = {}
+    for c in current:
+        pops[c['Country Code']] = int(c['Value'])
+
+    return pops

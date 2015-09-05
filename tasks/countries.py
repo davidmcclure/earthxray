25c4fe8d0a7248679e6dc1d5da6fea93234e3085
David McClure <davidwilliammcclure@gmail.com>
2015-09-04 17:13:16 -0700

    Strip out the OSM-backed label merging logic in the tasks.

--- tasks/countries.py
+++ tasks/countries.py
@@ -7,64 +7,37 @@ from tasks import utils
 
 
 @task
-def data():
+def borders():
 
     """
-    Write country borders and label points.
+    Write country borders.
     """
 
-    anchors = {}
+    # TODO|dev
+    borders = open_borders()
+    write_countries(borders)
 
-    # Map labels -> points.
 
-    with open('data/labels.geo.json', 'r') as fh:
+def open_borders():
 
-        labels = json.loads(fh.read())
+    """
+    Open the borders GeoJSON.
 
-        for f in labels['features']:
-
-            # Break if no English name.
-            name = f['properties'].get('name:en')
-            if not name: continue
-
-            lon = f['geometry']['coordinates'][0]
-            lat = f['geometry']['coordinates'][1]
-
-            # Map name -> XYZ point.
-            point = utils.lon_lat_to_xyz(lon, lat)
-            anchors[name] = point
-
-    countries = []
-
-    # Merge labels with borders, 3D-ify points.
+    Returns: dict
+    """
 
     with open('data/borders.geo.json', 'r') as fh:
+        return json.loads(fh.read())
 
-        borders = json.loads(fh.read())
 
-        for f in borders['features']:
+def write_countries(data):
 
-            country = {
-                'name': f['properties']['name'],
-                'points': [],
-            }
+    """
+    Write the countries GeoJSON.
 
-            # Set the label point.
-            country['anchor'] = anchors.get(country['name'])
+    Args:
+        data (dict)
+    """
 
-            # Polygons:
-            if f['geometry']['type'] == 'Polygon':
-                for p in f['geometry']['coordinates']:
-                    country['points'].append(utils.threedify(p))
-
-            # MultiPolygons:
-            elif f['geometry']['type'] == 'MultiPolygon':
-                for mp in f['geometry']['coordinates']:
-                    for p in mp:
-                        country['points'].append(utils.threedify(p))
-
-            countries.append(country)
-
-    # Write the payload.
     with open('src/javascripts/data/countries.json', 'w') as fh:
-        json.dump(countries, fh, sort_keys=True)
+        json.dump(data, fh, sort_keys=True)

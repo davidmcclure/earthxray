c208f74251c2fa663b2d108d9fe29da8b54cd3d4
David McClure <davidwilliammcclure@gmail.com>
2015-09-17 08:52:55 -0700

    Ready for improved zoom-in step.

--- src/javascripts/views/zoom.js
+++ src/javascripts/views/zoom.js
@@ -19,73 +19,23 @@ export default class extends Step {
    * @return {Promise}
    */
   start() {
-
-    // Get the swivel destination point.
-
-    let d = this.camera.position.z + (opts.earth.radius*1.5);
-    let r = d/2;
-
-    let [gx, gy, gz] = this.shared.location;
-    let cz = this.camera.position.z - r;
-
-    let a = gx*gx + gy*gy + gz*gz;
-    let b = 2*gz*-cz;
-    let c = cz*cz - r*r;
-
-    let t = (-b + Math.sqrt(b*b - 4*a*c)) / (2*a);
-
-    let x = t*gx;
-    let y = t*gy;
-    let z = t*gz;
-
-    // Get start and end coordinates.
-
-    let [lon1, lat1] = utils.xyzToLonLat(
-      this.camera.position.x,
-      this.camera.position.y,
-      this.camera.position.z-cz,
-      r
-    );
-
-    let [lon2, lat2] = utils.xyzToLonLat(x, y, z-cz, r);
-
-    // Generate a spline for the swivel.
-
-    let points = [];
-    _.times(21, i => {
-
-      let [lon, lat] = utils.intermediatePoint(
-        lon1, lat1, lon2, lat2, i/20
-      );
-
-      let [x, y, z] = utils.lonLatToXYZ(lon, lat, r);
-      points.push(new THREE.Vector3(x, y, z+cz));
-
-    });
-
-    let spline = new THREE.SplineCurve3(points);
-
-    // Create the splines.
-
-    let swivel = new TWEEN.Tween()
-      .to(null, 3000)
-      .easing(TWEEN.Easing.Quadratic.Out)
-      .onUpdate(f => {
-        this.camera.position.copy(spline.getPoint(f))
-        this.camera.lookAt(new THREE.Vector3(0, 0, 0));
-      });
-
-    let zoom = new TWEEN.Tween(this.camera.position)
-      .to({ x:gx, y:gy, z:gz }, 2000)
-      .easing(TWEEN.Easing.Quadratic.Out);
-
-    // Run the animation.
-
     return new Promise(resolve => {
-      zoom.onComplete(resolve);
-      swivel.chain(zoom).start();
-    });
 
+      let [x, y, z] = this.shared.location;
+
+      new TWEEN.Tween(this.camera.position)
+
+        .to({ x:x, y:y, z:z }, 5000)
+        .easing(TWEEN.Easing.Quadratic.Out)
+
+        .onUpdate(() => {
+          this.camera.lookAt(new THREE.Vector3(0, 0, 0));
+        })
+
+        .onComplete(resolve)
+        .start();
+
+    });
   }
 
 

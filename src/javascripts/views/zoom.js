696c03fa56fbe1970f82d1fbb7603f51ad743771
David McClure <davidwilliammcclure@gmail.com>
2015-09-19 09:32:53 -0700

    Blocking in the shrinking-sphere zoom strategy.

--- src/javascripts/views/zoom.js
+++ src/javascripts/views/zoom.js
@@ -4,7 +4,10 @@ import _ from 'lodash';
 import TWEEN from 'tween.js';
 import THREE from 'three';
 import Promise from 'bluebird';
+
 import Step from './step';
+import * as utils from '../utils';
+import opts from '../opts.yml';
 
 
 export default class extends Step {
@@ -16,23 +19,40 @@ export default class extends Step {
    * @return {Promise}
    */
   start() {
+
+    // Camera coordinates.
+    let [x0, y0, z0] = this.camera.position.toArray();
+    let [lon0, lat0] = utils.xyzToLonLat(x0, y0, z0);
+
+    // GPS coordinates.
+    let [x1, y1, z1] = this.shared.location;
+    let [lon1, lat1] = utils.xyzToLonLat(x1, y1, z1);
+
+    let r0 = this.camera.position.z;
+    let r1 = opts.earth.radius * 1.5;
+    let dr = r1-r0;
+
+    let swivel = new TWEEN.Tween()
+      .to(null, 3000)
+      .easing(TWEEN.Easing.Quadratic.Out)
+      .onUpdate(f => {
+
+        let r = r0 + dr*f;
+
+        let [lon, lat] = utils.intermediatePoint(
+          lon0, lat0, lon1, lat1, f
+        );
+
+        let [x, y, z] = utils.lonLatToXYZ(lon, lat, r);
+        this.camera.position.set(x, y, z);
+        this.camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+      });
+
     return new Promise(resolve => {
-
-      let [x, y, z] = this.shared.location;
-
-      new TWEEN.Tween(this.camera.position)
-
-        .to({ x:x, y:y, z:z }, 5000)
-        .easing(TWEEN.Easing.Quadratic.Out)
-
-        .onUpdate(() => {
-          this.camera.lookAt(new THREE.Vector3(0, 0, 0));
-        })
-
-        .onComplete(resolve)
-        .start();
-
+      swivel.onComplete(resolve).start();
     });
+
   }
 
 

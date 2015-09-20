5f526fc799cbed787fb59f48593a801f966ef1f8
David McClure <davidwilliammcclure@gmail.com>
2015-09-20 12:58:03 -0700

    Stubbing in a <GPSError /> component.

--- src/javascripts/components/app.js
+++ src/javascripts/components/app.js
@@ -3,11 +3,13 @@
 import { connect } from 'react-redux';
 import React, { Component } from 'react';
 import Scene from './scene';
+import GPSError from './gps-error';
 import Center from './center';
 
 
 @connect(state => ({
-  started: state.xray.active
+  started: state.xray.active,
+  gpsError: state.errors.gps,
 }))
 export default class extends Component {
 
@@ -16,16 +18,21 @@ export default class extends Component {
    * Render the top-level application structure.
    */
   render() {
+
     return (
       <div className="wrapper">
 
         <Scene />
+
+        // GPS error modal.
+        {this.props.gpsError ? <GPSError /> : null}
 
         // Show center when VR is active.
         {this.props.started ? <Center /> : null}
 
       </div>
     );
+
   }
 
 
--- src/javascripts/reducers/index.js
+++ src/javascripts/reducers/index.js
@@ -2,8 +2,10 @@
 
 import { combineReducers } from 'redux';
 import xray from './xray';
+import errors from './errors';
 
 
 export default combineReducers({
   xray,
+  errors,
 });

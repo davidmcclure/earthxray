c6ea78f18bedb325c3663d87dbcab58a7ef56b32
David McClure <davidwilliammcclure@gmail.com>
2015-09-22 08:09:09 -0700

    Breaks out a separate <Errors /> component.

--- src/javascripts/components/app.js
+++ src/javascripts/components/app.js
@@ -3,14 +3,12 @@
 import { connect } from 'react-redux';
 import React, { Component } from 'react';
 import Scene from './scene';
-import GPSError from './gps-error';
-import OrientationError from './orientation-error';
+import Errors from './errors';
 import Center from './center';
 
 
 @connect(state => ({
   started: state.xray.active,
-  errors: state.errors,
 }))
 export default class extends Component {
 
@@ -20,20 +18,13 @@ export default class extends Component {
    */
   render() {
 
-    let gpsError = this.props.errors.gps ?
-      <GPSError /> : null;
-
-    let orientationError = this.props.errors.orientation ?
-      <OrientationError /> : null;
-
     let center = this.props.started ?
       <Center /> : null;
 
     return (
       <div className="wrapper">
         <Scene />
-        {gpsError}
-        {orientationError}
+        <Errors />
         {center}
       </div>
     );

7cbdb173da227671217caa7036202f3de33077d0
David McClure <davidwilliammcclure@gmail.com>
2015-09-22 08:20:29 -0700

    Ready for <Scene /> refactor.

--- src/javascripts/components/scene.js
+++ src/javascripts/components/scene.js
@@ -3,59 +3,15 @@
 import $ from 'jquery';
 import React, { Component, findDOMNode } from 'react';
 
-import Scene from '../scene/scene';
-import Startup from '../scene/startup';
-import Zoom from '../scene/zoom';
-import Xray from '../scene/xray';
-
-import store from '../store';
-import { startXray } from '../actions/xray';
-import { showGPSError, showOrientationError } from '../actions/errors';
-import { GPSError, OrientationError } from '../errors';
-
 
 export default class extends Component {
 
 
   /**
-   * Run the scene manager.
+   * Start the shared scene components.
    */
   componentDidMount() {
-
-    let scene = new Scene(findDOMNode(this));
-
-    let startup = new Startup(scene);
-    let zoom = new Zoom(scene);
-    let xray = new Xray(scene);
-
-    // Render scene.
-    startup.start()
-
-      // Zoom to location.
-      .then(() => {
-        return zoom.start();
-      })
-
-      // Start VR.
-      .then(() => {
-        return xray.start();
-      })
-
-      // Notify started.
-      .then(() => {
-        store.dispatch(startXray());
-      })
-
-      // No GPS.
-      .catch(GPSError, err => {
-        store.dispatch(showGPSError());
-      })
-
-      // No accelerometer.
-      .catch(OrientationError, err => {
-        store.dispatch(showOrientationError());
-      });
-
+    // TODO
   }
 
 

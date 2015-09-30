2d4857ad52ad276725bc6f682fd4bd82c38f8c30
David McClure <davidwilliammcclure@gmail.com>
2015-09-30 13:50:38 -0700

    Markup and styling for error overlays.

--- src/javascripts/components/gps-error.js
+++ src/javascripts/components/gps-error.js
@@ -12,7 +12,11 @@ export default class extends Component {
   render() {
     return (
       <div className="error">
-        <h1>No GPS!</h1>
+        <div className="error-content animated flipInX">
+          <i className="fa fa-exclamation-circle"></i>
+          <h1>Whoops, can't get a GPS location</h1>
+          <p>Try it on your phone!</p>
+        </div>
       </div>
     );
   }
--- src/javascripts/components/orientation-error.js
+++ src/javascripts/components/orientation-error.js
@@ -11,7 +11,13 @@ export default class extends Component {
    */
   render() {
     return (
-      <h1>No accelerometer!</h1>
+      <div className="error">
+        <div className="error-content animated flipInX">
+          <i className="fa fa-exclamation-circle"></i>
+          <h1>Whoops, no accelerometer</h1>
+          <p>Try it on your phone!</p>
+        </div>
+      </div>
     );
   }
 

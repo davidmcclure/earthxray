5eb24bf61eb0d39c60d314122644efec1b02e83f
David McClure <davidwilliammcclure@gmail.com>
2015-10-02 09:12:39 -0700

    Moves the info markup back into <Nav />, wires up the toggle listener on the 'Got it' button.

--- src/javascripts/components/nav.js
+++ src/javascripts/components/nav.js
@@ -5,7 +5,7 @@ import React, { Component } from 'react';
 import classNames from 'classnames';
 import Tappable from 'react-tappable';
 import * as actions from '../actions/nav';
-import Info from './info';
+import Overlay from './overlay';
 
 
 @connect(
@@ -20,6 +20,23 @@ export default class extends Component {
    */
   render() {
 
+    let info = null;
+
+    if (this.props.active) {
+      info = (
+        <Overlay className="info">
+
+          <h1>Earth Xray</h1>
+          <p>See through the world!</p>
+
+          <Tappable onTap={this.props.toggleNav}>
+            <button className="btn btn-default btn-lg">Got it</button>
+          </Tappable>
+
+        </Overlay>
+      );
+    }
+
     let cx = classNames('nav', {
       active: this.props.active
     });
@@ -31,7 +48,7 @@ export default class extends Component {
           <i className="toggle fa fa-bars"></i>
         </Tappable>
 
-        {this.props.active ? <Info /> : null}
+        {info}
 
       </div>
     );

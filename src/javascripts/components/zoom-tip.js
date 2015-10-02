4474c593339c30936bdfecc203bb61b118ba2698
David McClure <davidwilliammcclure@gmail.com>
2015-10-02 14:34:01 -0700

    Set a 'pulse' interval in <ZoomTip />.

--- src/javascripts/components/zoom-tip.js
+++ src/javascripts/components/zoom-tip.js
@@ -1,9 +1,33 @@
 
 
-import React, { Component } from 'react';
+import React, { Component, findDOMElement } from 'react';
 
 
 export default class extends Component {
+
+
+  /**
+   * Set the pulse interval.
+   */
+  componentDidMount() {
+    this.interval = setInterval(this.pulse, 300);
+  }
+
+
+  /**
+   * Clear the pulse interval.
+   */
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+
+  /**
+   * Pulse the tip.
+   */
+  pulse() {
+    console.log('pulse');
+  }
 
 
   /**

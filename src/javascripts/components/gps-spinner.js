214ed73d2aa391311ab07821ddaf7c54d34a1d25
David McClure <davidwilliammcclure@gmail.com>
2015-10-13 22:41:08 -0700

    Ready for spinner rendering conditions.

--- src/javascripts/components/gps-spinner.js
+++ src/javascripts/components/gps-spinner.js
@@ -7,14 +7,12 @@ export default class extends Component {
 
 
   /**
+   * TODO
    * Render the GPS spinner.
    */
   render() {
     return (
-      <div className="spinner">
-        <i className="fa fa-spin fa-spinner"></i>
-        <p>Getting location...</p>
-      </div>
+      <p>Getting location...</p>
     );
   }
 

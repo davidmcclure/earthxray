b49800b76164c0eeb01386822ae2a54fa4545f82
David McClure <davidwilliammcclure@gmail.com>
2015-10-13 22:17:09 -0700

    Stubbing in a <GPSSpinner /> component.

---
+++ src/javascripts/components/gps-spinner.js
@@ -0,0 +1,22 @@
+
+
+import React, { Component } from 'react';
+
+
+export default class extends Component {
+
+
+  /**
+   * Render the GPS spinner.
+   */
+  render() {
+    return (
+      <div className="gps-spinner">
+        <i className="fa fa-spin fa-spinner"></i>
+        <p>Getting location...</p>
+      </div>
+    );
+  }
+
+
+}

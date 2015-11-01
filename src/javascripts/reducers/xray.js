91c986ec194699a8bf251dbe4718bfc31bc44211
David McClure <davidwilliammcclure@gmail.com>
2015-10-31 23:52:55 -0700

    Listen for START/END_CALIBRATING in the xray reducer.

--- src/javascripts/reducers/xray.js
+++ src/javascripts/reducers/xray.js
@@ -5,11 +5,14 @@ import { createReducer } from '../utils';
 import {
   START_XRAY,
   ZOOM_XRAY,
+  START_CALIBRATING,
+  END_CALIBRATING,
 } from '../constants';
 
 
 const initialState = {
   active: false,
+  isCalibrating: false,
   hasZoomed: false,
 };
 
@@ -22,6 +25,14 @@ const handlers = {
 
   [ZOOM_XRAY]: () => ({
     hasZoomed: true
+  }),
+
+  [START_CALIBRATING]: () => ({
+    isCalibrating: true
+  }),
+
+  [END_CALIBRATING]: () => ({
+    isCalibrating: false
   }),
 
 };

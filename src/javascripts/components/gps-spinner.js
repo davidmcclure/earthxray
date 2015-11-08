53fc9f3c6799fd8fa5d4f9c847382acb7417861e
David McClure <davidwilliammcclure@gmail.com>
2015-11-07 16:08:29 -0800

    Use #gps-spinner, not .spinner.

--- src/javascripts/components/gps-spinner.js
+++ src/javascripts/components/gps-spinner.js
@@ -13,7 +13,7 @@ export default class extends Component {
    */
   render() {
     return (
-      <div className="spinner">
+      <div id="gps-spinner">
         <Static html={barsSVG} />
         <p>Getting location...</p>
       </div>
